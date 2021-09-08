11d497296a86b1ecfde6c7f92ce73b18e6f1d93d
syurede <8118055@ed.tus.ac.jp>
2021-09-08 14:07:13 +0900

    [AC] abc217 abc217_a

---
+++ atcoder.jp/abc217/abc217_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+//#include <string>
+using namespace std;
+
+int main(){
+    string S, T;
+    
+    cin >> S >> T;
+    if(S < T) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
