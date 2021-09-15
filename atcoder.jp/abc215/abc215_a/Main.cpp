980e39367ea6c6f4331f8fb6e509d4b1b61f7863
syurede <8118055@ed.tus.ac.jp>
2021-09-15 11:41:41 +0900

    [AC] abc215 abc215_a

---
+++ atcoder.jp/abc215/abc215_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string S, T = "Hello,World!";
+    cin >> S;
+    if(S == T) cout << "AC" << endl;
+    else cout << "WA" << endl;
+    return 0;
+}
