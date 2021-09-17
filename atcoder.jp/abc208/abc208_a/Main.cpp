40c07c7206c095cb98c1fe833f2c022585a2e1c8
syurede <8118055@ed.tus.ac.jp>
2021-09-17 14:05:18 +0900

    [AC] abc208 abc208_a

---
+++ atcoder.jp/abc208/abc208_a/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+    if(6 * A >= B && A <= B) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
