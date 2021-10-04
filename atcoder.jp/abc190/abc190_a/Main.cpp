9fd640f3121aa1e5e229ceeb2f0b6f9e97f0a878
syurede <8118055@ed.tus.ac.jp>
2021-10-04 19:47:40 +0900

    [AC] abc190 abc190_a

---
+++ atcoder.jp/abc190/abc190_a/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A, B, C;
+    cin >> A >> B >>C;
+    if(A == B && C == 0 || A < B) cout << "Aoki" <<endl;
+    else cout << "Takahashi" << endl;
+}
