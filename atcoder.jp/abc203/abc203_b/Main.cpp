a704ecc77d5b9d859a699bd829613f559e1d1167
syurede <8118055@ed.tus.ac.jp>
2021-10-15 11:55:56 +0900

    [AC] abc203 abc203_b

---
+++ atcoder.jp/abc203/abc203_b/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, K;
+    cin >> N >> K;
+    long long ans = 0; 
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= K; j++){
+            ans += i * 100 + j;
+        }
+    }
+    cout << ans << endl;
+}
