d783d501db59a3c5d14e694548b75569e10eae37
syurede <8118055@ed.tus.ac.jp>
2021-10-23 21:04:07 +0900

    [AC] abc224 abc224_a

---
+++ atcoder.jp/abc224/abc224_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string S;
+    cin >> S;
+    int si = S.size();
+    if(S[si-1] == 'r') cout << "er" << endl;
+    else cout << "ist" << endl;
+}
