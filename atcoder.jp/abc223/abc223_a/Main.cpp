509f24f1fcddc46c46fe330d6d827273cfac3629
syurede <8118055@ed.tus.ac.jp>
2021-10-26 14:53:12 +0900

    [AC] abc223 abc223_a

---
+++ atcoder.jp/abc223/abc223_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int X;
+    cin >> X;
+    if(X > 1 && X % 100 == 0){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
