8a700ed9711113f3c595c8a4cbda454cb85b8459
syurede <8118055@ed.tus.ac.jp>
2021-11-16 10:20:57 +0900

    [AC] abc016 abc016_1

---
+++ atcoder.jp/abc016/abc016_1/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int M, D;
+    cin >> M >> D;
+    if(M % D != 0){
+        cout << "NO" << endl;
+    }else{
+        cout << "YES" << endl;
+    }
+}
