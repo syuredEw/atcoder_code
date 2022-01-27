9c4dcc07955a7f8ccc6e557b53b5703850eab0be
syurede <8118055@ed.tus.ac.jp>
2022-01-27 21:16:30 +0900

    [AC] abc164 abc164_b

---
+++ atcoder.jp/abc164/abc164_b/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    while (1)
+    {
+        C -= B;
+        if(C <= 0){
+            cout << "Yes" << endl;
+            break;
+        }
+        A -= D;
+        if(A <= 0){
+            cout << "No" << endl;
+            break;
+        }
+    }
+    
+}
